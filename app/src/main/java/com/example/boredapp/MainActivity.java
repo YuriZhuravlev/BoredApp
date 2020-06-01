9830c3477770a4aaf4136c61064d9b483d434b9e
Agafiy_Poluchlebnik <zhuravlevyuri2000@gmail.com>
2020-06-01 23:40:51 +0300

    первоначальная вёрстка фрагмента

--- app/src/main/java/com/example/boredapp/MainActivity.java
+++ app/src/main/java/com/example/boredapp/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.boredapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.example.boredapp.ui.fragments.ActivityFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container,new ActivityFragment())
+                    .addToBackStack("")
+                    .commit();
+        }
     }
 }
